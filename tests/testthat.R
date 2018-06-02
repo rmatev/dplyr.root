6260b3372232ed8ba4da1b2b8f1f1673cec347b7
Rosen Matev <rosen.matev@cern.ch>
2018-06-03 00:08:23 +0200

    Add a few simple tests

---
+++ tests/testthat.R
@@ -0,0 +1,4 @@
+library(testthat)
+library(dplyr.root)
+
+test_check("dplyr.root")
---
+++ tests/testthat/test-tbl-root.R
@@ -0,0 +1,37 @@
+context("tbl-root")
+library(dplyr)
+
+filename <- system.file("examples/exampleEs.root", package = "RootTreeToR")
+data <- tbl_rootchain(filename, 'exampleEs')
+
+test_that("filter works", {
+  expect_identical(
+    filter(collect(data), eCh > 0, px > 0),
+    collect(filter(data, eCh > 0, px > 0))
+  )
+})
+
+test_that("globals don't mask data variables", {
+  pz <- function() { stop("This should never be evaluated") }
+  data <- collect(filter(data, pz > 180))
+  expect_true(T)
+})
+
+test_that("empty collect works", {
+  res <- collect(filter(data, pz > 1000))
+  expect_equal(nrow(res), 0)
+})
+
+test_that("select works", {
+  expect_named(
+    collect(select(data, pz, px)),
+    c("pz", "px")
+  )
+})
+
+test_that("mutate works", {
+  expect_identical(
+    collect(mutate(data, pt = sqrt(px^2 + py^2))),
+    mutate(collect(data), pt = sqrt(px^2 + py^2))
+  )
+})
